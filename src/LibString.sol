72fe17104e7e460c25a447d28cc8469ec4c0083b
James Wenzel <wenzel.james.r@gmail.com>
2024-01-26 18:23:47 -0800

    add libstring convenience

---
+++ src/LibString.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {LibString} from "solady/utils/LibString.sol";
+
+/// @notice convenience file for easily importing LibString without specifying
+/// another dependency and remapping
